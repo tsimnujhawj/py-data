468291026669acf13246bebca1b403a93c600068
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-04-19 14:05:45 -0500

    update

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ path = "./data/costs.csv"
 file = open(path, newline="")
 df = pd.read_csv(file)
 
+# Print averages
 print("October Average\n", df.loc[1:31].mean())
 print("\n")
 print("November Average\n", df.loc[32:].mean())
-
-df
